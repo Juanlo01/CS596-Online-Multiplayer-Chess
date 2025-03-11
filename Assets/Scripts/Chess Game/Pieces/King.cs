eb1f5797bbbc9ce8dff032f271f2379806ae2923
Juanlo01 <145627909+Juanlo01@users.noreply.github.com>
2025-03-10 18:27:36 -0700

    Online Chess
    Jess

---
+++ Assets/Scripts/Chess Game/Pieces/King.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King : Piece{
+    public override List<Vector2Int> SelectAvailableSquares(){
+        throw new System.NotImplementedException();
+    }
+}
